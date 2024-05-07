1d4151930c410e859a5ec249c4ce24df9b225a83
rdftgyhuy6 <gs1matutaru@hotmail.com>
2024-05-07 15:28:29 +0700

    hello

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function connect(root) {
-  if (!root) return root;
-  let levelStart = root;
-  while (levelStart) {
-    let curr = levelStart;
-    while (curr) {
-      if (curr.left) curr.left.next = curr.right;
-      if (curr.right && curr.next) curr.right.next = curr.next.left;
-      curr = curr.next;
-    }
-    levelStart = levelStart.left;
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
   }
-  return root;
+  return -1;
 }
