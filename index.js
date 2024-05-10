63acfde2aa0fa13b2256043bd0e9428482344b32
rdftgyhuy6 <gs1matutaru@hotmail.com>
2024-05-11 03:01:53 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-function jump(nums) {
-  let jumps = 0;
-  let currentEnd = 0;
-  let farthest = 0;
-  for (let i = 0; i < nums.length - 1; i++) {
-    farthest = Math.max(farthest, i + nums[i]);
-    if (i === currentEnd) {
-      jumps++;
-      currentEnd = farthest;
+function numSquares(n) {
+  const dp = new Array(n + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
     }
   }
-  return jumps;
+  return dp[n];
 }
