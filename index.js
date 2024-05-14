a7e24dc0fb087ba36db08997acea437ddb911006
rdftgyhuy6 <gs1matutaru@hotmail.com>
2024-05-14 12:24:57 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const insertionSortRecursive = (arr, n = arr.length) => {
-  if (n <= 1) {
-    return arr;
-  }
-  insertionSortRecursive(arr, n - 1);
-  const last = arr[n - 1];
-  let j = n - 2;
-  while (j >= 0 && arr[j] > last) {
-    arr[j + 1] = arr[j];
-    j--;
-  }
-  arr[j + 1] = last;
+const dropWhile = (arr, func) => {
+  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
   return arr;
 };
