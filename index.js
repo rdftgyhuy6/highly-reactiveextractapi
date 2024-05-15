5721cc233300e0022813cb0d730fe7eaea05aac2
rdftgyhuy6 <gs1matutaru@hotmail.com>
2024-05-15 13:33:48 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const dropWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
-  return arr;
-};
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 1; i <= n + 1; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+}
